5e5b8cee40f50c35347f5689d941fde56553f83a
Jane Suh <sourcherryyogurt@gmail.com>
2020-04-30 18:59:29 -0400

    Added six helper methods, tested CRUD operations on schemes endpoint

---
+++ schemes/scheme-model.js
@@ -0,0 +1,38 @@
+const db = require('../data/config');
+
+function find() {
+    return db('schemes');
+};
+
+function findById(id) {
+    return db('schemes').where({ id }).first();
+};
+
+function findSteps(id) {
+    return db('steps as st')
+        .join('schemes as s', 's.id', 'st.scheme_id')
+        .where('s.id', id)
+        .select('st.id', 's.scheme_name', 'st.step_number', 'st.instructions')
+        .orderBy('st.step_number');
+};
+
+function add(scheme) {
+    return db('schemes').insert(scheme, 'id');
+};
+
+function update(changes, id) {
+    return db('schemes').where({ id }).update(changes);
+};
+
+function remove(id) {
+    return db('schemes').where({ id }).del();
+};
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+};
